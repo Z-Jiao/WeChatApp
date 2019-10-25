765f19b010fcaf853871bf8e2038448d2cac39cb
Jiaoyang-168 <51904882+Jiaoyang-168@users.noreply.github.com>
2019-10-25 18:06:56 +0800

    init WeChatApp

---
+++ src/main/java/com/wechatapp/WechatappApplication.java
@@ -0,0 +1,13 @@
+package com.wechatapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WechatappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WechatappApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wechatapp/WechatappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wechatapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WechatappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
