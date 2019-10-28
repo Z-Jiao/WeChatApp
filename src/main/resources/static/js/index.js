029f573d9f4047f512235703578bb23babed4535
zhangyixin9 <13721658666@163.com>
2019-10-28 17:01:20 +0800

    wechatapp10

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -17,4 +17,17 @@ $(function () {
     $(".nav-top>.row>.row1>ul>li:nth-child(3)").mouseleave(function () {
         $(".ulhidden2").hide();
     })
+
+    $(window).resize(function () {          //当浏览器大小变化时
+        if($(window).width() < 650 ) {
+            console.log($(window).width());
+            $(".indexbox").hide();
+           /* $(".indexbox>.logindiv").show();*/
+            $(".indexbox2").show();
+        }else if($(window).width() > 650){
+            $(".indexbox").show();
+            $(".indexbox2").hide();
+        }
+    });
+
 })
