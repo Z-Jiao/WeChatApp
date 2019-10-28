a4f1a7b0e2b74dcbfec0e9a182777d113dd1cc52
zhangyixin9 <13721658666@163.com>
2019-10-28 10:30:37 +0800

    wechatapp09

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,16 +1,20 @@
 $(function () {
    var flag=true;
-    $(".nav-left>ul>li:nth-child(2)").click(function () {
+    $(".nav-left>ul>li:nth-child(2)>a").click(function () {
         if(flag){
             $(".nav-left>ul>li:nth-child(2)").css("backgroundColor","#203040");
             $(".ulhidden").show();
+            console.log(111);
         }else {
             $(".nav-left>ul>li:nth-child(2)").css("backgroundColor","#2f4050");
             $(".ulhidden").hide();
         }
        flag=!flag;
     })
-    $(".nav-top>.row ul>li:nth-child(3)").mousemove(function () {
-
+    $(".nav-top>.row>.row1>ul>li:nth-child(3)").mouseenter(function () {
+        $(".ulhidden2").show();
+    })
+    $(".nav-top>.row>.row1>ul>li:nth-child(3)").mouseleave(function () {
+        $(".ulhidden2").hide();
     })
 })
